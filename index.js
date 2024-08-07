d26f15e471ebe103daf30c593d45223ff4d6c6e0
Ezequiel-Front-End <ezequielfernandes912@gmail.com>
2024-08-07 15:57:33 -0300

    modificando

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Ezequiel dos Santos!');
+// Middleware para parsear o corpo das requisições
+app.use(bodyParser.json());
+
+// Configurar o Nodemailer
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: true, // Use `true` for port 465, `false` for all other ports
+    auth: {
+        user: "ezequielfernandes912@gmail.com",
+        pass: "cnswjsgojezdzcvy",
+    },
 });
 
-app.post('/api/receive-object', (req, res) => {
-    const receivedObject = req.body;
-    console.log('Objeto recebido:', receivedObject);
+app.get('/', (req, res) => {
+    res.send('API de envio de Email AbcTechnology');
+});
 
-    res.status(200).json({
-        message: 'Objeto recebido com sucesso',
-        receivedObject: receivedObject
+app.post('/api/send-email', (req, res) => {
+    // const { to, subject, text } = req.body;
+
+    const mailOptions = {
+        from: "ezequielfernandes912@gmail.com",
+        to: "ezequielfernandes912@gmail.com",
+        subject: "Testando o sistema de envio de email",
+        text: "testando..."
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).send(error.toString());
+        }
+
+        res.status(200).send('Email enviado: ' + info.response);
     });
 });
 
